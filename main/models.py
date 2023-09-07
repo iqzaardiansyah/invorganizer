919e7a58993047b6603ec999276de734722d52ae
iqzaardiansyah <iqzaardiansyah@gmail.com>
2023-09-07 13:54:03 +0700

    update

--- main/models.py
+++ main/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.urls import reverse
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from models import Item
+from .models import Item
 
 def show_main(request):
     context = {
