3ecd939f13d16c81c404c17cc7389ee6f5244f4b
iqzaardiansyah <iqzaardiansyah@gmail.com>
2023-09-07 14:34:44 +0700

    update

--- main/models.py
+++ main/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Item(models.Model):
-    name = models.CharField(max_length=255)
-    amount = models.IntegerField()
-    price = models.IntegerField()
-    description = models.TextField()
+    name = models.CharField(max_length=255, default='')
+    amount = models.IntegerField(default=0)
+    price = models.IntegerField(default=0)
+    description = models.TextField(default='')
 
     def __str__(self) :
         return self.name
