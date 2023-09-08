6c6513e7febf0e8b8298df90a94c8707407406c1
iqzaardiansyah <iqzaardiansyah@gmail.com>
2023-09-08 09:27:25 +0700

    update

--- main/views.py
+++ main/views.py
@@ -2,13 +2,11 @@ from django.shortcuts import render
 from .models import Item
 
 def show_main(request):
+    list = Item.objects.all().order_by("name")
     context = {
         'name': 'Iqza Ardiansyah',
-        'class': 'PBP F'
+        'class': 'PBP F',
+        'list' : list
     }
 
     return render(request, "main.html", context)
-
-def show_products(request):
-    data = {}
-    return render(request, "main.html", )
