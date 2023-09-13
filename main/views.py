5421e06b9982c04b8f4825547871082f3609c2f3
Iqza Ardiansyah <124988403+iqzaardiansyah@users.noreply.github.com>
2023-09-13 11:26:54 +0700

    Update views.py

--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ from .models import Item
 def show_main(request):
     list = Item.objects.all().order_by("name")
     context = {
-        'name': 'Iqzaa Ardiansyah',
+        'name': 'Iqza Ardiansyah',
         'class': 'PBP F',
         'list' : list
     }
