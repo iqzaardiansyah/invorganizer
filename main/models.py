a0f2568e36826a0628c9b19230d435472c9591b2
iqzaardiansyah <iqzaardiansyah@gmail.com>
2023-09-20 18:00:42 +0700

    Tugas 4

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Item(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, default='')
     amount = models.IntegerField(default=0)
     price = models.IntegerField(default=0)
--- main/urls.py
+++ main/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('json/', show_xml, name='show_json'), 
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
