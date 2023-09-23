315bff50487dd426c1d90f1e10088701d5f5a4d3
iqzaardiansyah <iqzaardiansyah@gmail.com>
2023-09-23 19:38:08 +0700

    update mengikuti announcement di discord

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,10 @@
 from django.forms import ModelForm
-from .models import Item
+from main.models import Item
+from django import forms
+from django.contrib.auth.models import User
 
-class ItemForm(ModelForm):
+class ProductForm(ModelForm):
     class Meta:
         model = Item
-        fields = ["name", "amount", "price", "description"]
+        fields = ["name", "amount", "price", "description", "user"]
+    user = forms.ModelChoiceField(queryset=User.objects.all(), required=True)
--- main/models.py
+++ main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Item(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
     name = models.CharField(max_length=255, default='')
     amount = models.IntegerField(default=0)
     price = models.IntegerField(default=0)
